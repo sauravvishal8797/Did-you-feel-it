28373412a0cff4e982951cbb774b8e380bb35524
Saurav Vishal <sauravvishal8797@gmail.com>
2017-04-28 21:12:05 +0530

    Added the Async class to run the network tasks in a background thread

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,8 +15,16 @@
  */
 package com.example.android.didyoufeelit;
 
+import static com.example.android.didyoufeelit.Utils.LOG_TAG;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 /**
@@ -33,13 +41,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
-    }
+        
+           }
 
     /**
      * Update the UI with the given earthquake information.
@@ -54,4 +57,21 @@ public class MainActivity extends AppCompatActivity {
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
     }
-}
+
+    private class EarthQuakeAsyncTask extends AsyncTask<String, Void, Event>{
+        @Override protected Event doInBackground(String... strings) {
+            Event earthquake = Utils.fetchEarthquakeData(strings[0]);
+            return earthquake;
+
+        }
+
+        @Override protected void onPostExecute(Event event) {
+            updateUi(event);
+        }
+    }
+
+
+        }
+
+
+
