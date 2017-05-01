95bec15c4d1e06380a67587884075dfb03a590cb
Saurav Vishal <sauravvishal8797@gmail.com>
2017-05-01 11:30:35 +0530

    Fixed an app crash

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -44,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EarthQuakeAsyncTask earthQuakeAsyncTask = new EarthQuakeAsyncTask();
-        earthQuakeAsyncTask.execute();
+        earthQuakeAsyncTask.execute(USGS_REQUEST_URL);
            }
 
     /**
